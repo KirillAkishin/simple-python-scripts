293774d8fa1a653ea0eecaa806148dde553cf8c3
KirillAkishin <akishin.ka@yandex.ru>
2025-03-14 17:14:16 +0300

    simple filesystem scripts - added

---
+++ filesystem.py
@@ -0,0 +1,48 @@
+def rel_walk(root=".", start=None):
+    import os
+    import os.path
+    for path, _, files in os.walk(root):
+        for name in files:
+            filename = os.path.join(path, name)
+            relative_filename = os.path.relpath(filename, start=start)
+            yield relative_filename
+
+def walk(root="."):
+    import os
+    import os.path
+    for path, _, files in os.walk(root):
+        for name in files:
+            filename = os.path.join(path, name)
+            yield filename
+
+def count(root="."):
+    import os
+    import os.path
+    cnt = 0
+    for _, _, files in os.walk(root):
+        for _ in files:
+            cnt += 1
+    return cnt
+
+def total_size(root="."):
+    import os
+    import os.path
+    ttl_size = 0
+    for path, _, files in os.walk(root):
+        for name in files:
+            ttl_size += os.path.getsize(os.path.join(path, name))
+    return ttl_size
+
+def filesize(filename):
+    from os import stat
+    size = stat(filename).st_size
+    if (size // 1024) == 0:
+        return f"{size} B"
+    size /= 1024
+    if (size // 1024) == 0:
+        return f"{size:.1f} KiB"
+    size /= 1024
+    if (size // 1024) == 0:
+        return f"{size:.1f} MiB"
+    size /= 1024
+    return f"{size:.1f} GiB"
